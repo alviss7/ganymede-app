45b074316865c6071080b2d8e72eb64a83f2e81f
Wk <kevinturmel@gmail.com>
2024-11-08 02:02:51 +0100

    feat: click on images and quest

--- src/components/guide-frame.tsx
+++ src/components/guide-frame.tsx
@@ -5,6 +5,8 @@ import { useSuspenseQuery } from '@tanstack/react-query'
 import { confQuery } from '@/queries/conf.query'
 import { copyPosition } from '@/lib/copy-position'
 import { useOpenGuideLink } from '@/mutations/open-guide-link.mutation'
+import { cn } from '@/lib/utils'
+import { writeText } from '@tauri-apps/plugin-clipboard-manager'
 
 export function GuideFrame({
   className,
@@ -40,7 +42,7 @@ export function GuideFrame({
             {prefix}
             <button
               type="button"
-              className="inline-flex text-left font-semibold hover:saturate-50 focus-visible:saturate-[12.5%]"
+              className="inline-flex font-semibold hover:saturate-50 focus:saturate-[12.5%]"
               onClick={async () => {
                 await copyPosition(Number.parseInt(posX, 10), Number.parseInt(posY, 10), conf.data)
               }}
@@ -53,6 +55,67 @@ export function GuideFrame({
       }
 
       if (domNode.type === 'tag') {
+        if (domNode.name === 'p' && domNode.children.length === 0) {
+          const countEmptyP = (node: DOMNode | null): number => {
+            if (!node) return 0
+
+            if (node.type === 'tag' && node.name === 'p' && node.children.length === 0) {
+              return 1 + countEmptyP(node.next as DOMNode | null)
+            }
+
+            return 0
+          }
+
+          const countNextEmptyP = countEmptyP(domNode)
+
+          // disallow multiple empty p tags
+          if (countNextEmptyP > 1) {
+            return <></>
+          }
+
+          return <br />
+        }
+
+        if (
+          domNode.attribs['data-type'] === 'custom-tag' &&
+          (domNode.attribs.type === 'monster' || domNode.attribs.type === 'quest')
+        ) {
+          const name = domNode.attribs.name ?? ''
+          const { class: nodeClassName, ...restAttribs } = domNode.attribs
+
+          return (
+            <div {...restAttribs} className={cn('inline-flex hover:saturate-150 focus:saturate-[25%]', nodeClassName)}>
+              {domToReact([domNode.children[0]] as DOMNode[], options)}
+              <button
+                type="button"
+                onClick={async () => {
+                  await writeText(name)
+                }}
+              >
+                {name}
+              </button>
+            </div>
+          )
+        }
+
+        if (domNode.name === 'img') {
+          const imgSrc = domNode.attribs.src ?? ''
+
+          return (
+            <button
+              type="button"
+              className="inline-flex self-end"
+              onClick={() => {
+                if (imgSrc !== '' && imgSrc.startsWith('http')) {
+                  openGuideLink.mutate(imgSrc)
+                }
+              }}
+            >
+              <img {...domNode.attribs} draggable={false} className="select-none" />
+            </button>
+          )
+        }
+
         if (domNode.name === 'a') {
           const href = domNode.attribs.href ?? ''
 
@@ -60,7 +123,7 @@ export function GuideFrame({
             <button
               data-href={href}
               type="button"
-              className="inline-flex text-left"
+              className="inline-flex"
               onClick={() => {
                 if (href !== '' && href.startsWith('http')) {
                   openGuideLink.mutate(href)
