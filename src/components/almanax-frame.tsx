2c8850a680a426dcdfd9e9a4236b2aecb8f847e1
Wk <kevinturmel@gmail.com>
2024-11-08 02:57:25 +0100

    feat: almanax kamas and xp icon

--- src/components/almanax-frame.tsx
+++ src/components/almanax-frame.tsx
@@ -1,6 +1,8 @@
 import { almanaxQuery } from '@/queries/almanax.query'
 import { useQuery } from '@tanstack/react-query'
 import { LoaderIcon } from 'lucide-react'
+import kamasIcon from '@/assets/kamas.webp'
+import xpIcon from '@/assets/xp.webp'
 
 export function AlmanaxFrame() {
   const almanax = useQuery(almanaxQuery)
@@ -17,8 +19,14 @@ export function AlmanaxFrame() {
               {almanax.data.quantity.toLocaleString()}x {almanax.data.name}
             </span>
           </div>
-          <div className="text-center text-xs">{almanax.data.experience.toLocaleString()} EXP</div>
-          <div className="text-center text-xs">{almanax.data.kamas.toLocaleString()} KAMAS</div>
+          <div className="flex items-center justify-center gap-2 text-xs">
+            <img src={xpIcon} className="h-4 select-none" draggable={false} />
+            <span>{almanax.data.experience.toLocaleString()} </span>
+          </div>
+          <div className="flex items-center justify-center gap-2 text-xs">
+            <img src={kamasIcon} className="h-4 select-none" draggable={false} />
+            <span>{almanax.data.kamas.toLocaleString()} </span>
+          </div>
           <div
             className="prose-sm text-center text-xs leading-4"
             dangerouslySetInnerHTML={{ __html: almanax.data.bonus }}
